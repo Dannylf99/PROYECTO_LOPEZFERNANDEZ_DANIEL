6284f6258a77623a843b4e92ecb3286f54e0b927
Dannylf99 <WJQ25674@educastur.es>
2026-04-23 17:08:01 +0200

    Arreglado el pom y el proyecto inicio

--- src/main/java/main/ProyectoLopezfernandezDanielApplication.java
+++ src/main/java/main/ProyectoLopezfernandezDanielApplication.java
@@ -1,22 +1,12 @@
 package main;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ProyectoLopezfernandezDanielApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProyectoLopezfernandezDanielApplication.class, args);
-    }
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @PostConstruct
-    public void init() {
-        System.out.println("HASH: " + passwordEncoder.encode("Prueba123!"));
     }
 }
